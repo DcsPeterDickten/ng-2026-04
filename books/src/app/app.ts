16cd112c7dea86eb3517293e811f9dd9936c5ed2
Peter Dickten <peter.dickten@dcs-fuerth.de>
2026-04-27 14:58:00 +0200

    book-list

--- books/src/app/app.ts
+++ books/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BookList } from "./books/book-list/book-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BookList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('books');
+  public name: string = 'Bücher'
 }
