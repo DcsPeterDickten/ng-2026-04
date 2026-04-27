3f6abb81caf383d0f46bf83c82bf9e456c5ba9b2
Peter Dickten <peter.dickten@dcs-fuerth.de>
2026-04-27 22:21:50 +0200

    property binding

--- books/src/app/app.ts
+++ books/src/app/app.ts
@@ -4,10 +4,11 @@ import { BookList } from "./books/book-list/book-list";
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, BookList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  public name: string = 'Bücher'
+  public name: string = 'Bücher';
 }
--- books/src/app/books/book-list/book-list.ts
+++ books/src/app/books/book-list/book-list.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './book-list.html',
   styleUrl: './book-list.css',
 })
-export class BookList {}
+export class BookList {
+  public breite = 70;
+  public books: any[] = [
+    { name: 'Angular 18 for Beginners', isbn: '1234567890', price: 10.99, rating: 3.5, 
+      coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._SY522_.jpg' },
+    { name: 'Angular 20 for Freaks', isbn: '1234567891', price: 12.99, rating: 3.6, coverUrl:'https://m.media-amazon.com/images/I/61IEJY3sWWL._AC_UY436_QL65_.jpg' },
+    { name: 'Angular 22 for Nerds', isbn: '1234567892', price: 14.99, rating: 3.9, coverUrl:'https://m.media-amazon.com/images/I/71le4bCnY1L._AC_UY436_QL65_.jpg' },
+  ];
+}
