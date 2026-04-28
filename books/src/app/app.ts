a3a231ffe26ef90682f98eb2505f3480c9ba27c7
Peter Dickten <peter.dickten@dcs-fuerth.de>
2026-04-28 12:53:43 +0200

    cleanup

--- books/src/app/app.ts
+++ books/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BookList } from "./books/book-list/book-list";
-import { Calculator } from './shared/calculator/calculator';
+// import { Calculator } from './shared/calculator/calculator';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BookList, Calculator],
+  imports: [RouterOutlet, BookList, ], // Calculator
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
