b953070685a212f0caf200ff409c22211f3b4208
Peter Dickten <peter.dickten@dcs-fuerth.de>
2026-04-28 11:43:50 +0200

    lifecycle-hooks

--- books/src/app/books/book-list/book-list.ts
+++ books/src/app/books/book-list/book-list.ts
@@ -1,24 +1,40 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe } from '@angular/common';
+import { Component, OnChanges, OnDestroy, OnInit, SimpleChange } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Book } from '../book';
 
 @Component({
   selector: 'book-list',
-  imports: [FormsModule],
+  imports: [FormsModule, CurrencyPipe, DecimalPipe],
   templateUrl: './book-list.html',
   styleUrl: './book-list.css',
+  //encapsulation: ViewEncapsulation.None
 })
-export class BookList {
+export class BookList implements OnInit, OnChanges, OnDestroy {
   public breite = 70;
-  public books: any[] = [
+  public books: Array<Book> = [
     { name: 'Angular 18 for Beginners', isbn: '1234567890', price: 10.99, rating: 3.5,
       coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._SY522_.jpg' },
     { name: 'Angular 20 for Freaks', isbn: '1234567891', price: 12.99, rating: 3.6, coverUrl:'https://m.media-amazon.com/images/I/61IEJY3sWWL._AC_UY436_QL65_.jpg' },
     { name: 'Angular 22 for Nerds', isbn: '1234567892', price: 14.99, rating: 3.9, coverUrl:'https://m.media-amazon.com/images/I/71le4bCnY1L._AC_UY436_QL65_.jpg' },
   ];
   public coverIsVisible = true;
-searchValue: any;
-  // public myVar : boolean | null = null;
-  // alter : 43 | 44 | 45 = 43;
+  public searchValue: string = '';
+
+  constructor() {
+    console.log('BookList constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('BookList ngOnInit');
+  }
+  ngOnChanges(changes: { [propName: string]: SimpleChange<any>; }): void {
+    console.log('BookList ngOnChanges', changes);
+  }
+  ngOnDestroy(): void {
+    console.log('BookList ngOnDestroy');
+  }
+
 
   toggleCover() {
     this.coverIsVisible = !this.coverIsVisible;
---
+++ books/src/app/books/book.ts
@@ -0,0 +1,7 @@
+export interface Book {
+  name: string;
+  isbn: string;
+  price: number;
+  rating: number;
+  coverUrl: string;
+}
